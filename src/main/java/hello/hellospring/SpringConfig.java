4f22a5dc7b05776af5bf96c690cf951864e378da
mun2a <mkyrjin@gmail.com>
2022-10-07 15:20:05 +0900

    jdbc

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,29 +1,44 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRespository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration      //한 개 이상의 @Bean을 제공하는 클래스에 명시하는 어노테이션으로,
                     // IOC Container 에게 해당 클래스가 Bean 으로 구성된 클래스라는 것을 알려줍니다.
 public class SpringConfig {
 /*    스프링 빈 등록 이미지
        -------스프링 컨테이너-------------------------------------
-      |  helloController -> memberService -> memberRepository  |
+      |  helloController -> memberService -> MemberRepository  |
        --------------------------------------------------------
-       memberService 와 memberRepository 가 스프링 컨테이너에 스프링 빈으로 등록되었다.
+       memberService 와 MemberRepository 가 스프링 컨테이너에 스프링 빈으로 등록되었다.
 */
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRespository());
+        return new MemberService(MemberRepository());
     }
 
     @Bean
-    public MemberRespository memberRespository(){
-        return new MemoryMemberRepository();
+    public MemberRepository MemberRepository(){
+
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,7 +1,7 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberRespository;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +12,11 @@ import java.util.Optional;
 //@Service
 public class MemberService {
 
-    private final MemberRespository memberRespository;
+    private final MemberRepository memberRepository;
 
 //    @Autowired
-    public MemberService(MemberRespository memberRespository){
-        this.memberRespository = memberRespository;
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
     /*
     * 생성자에 @Autowired를 사용하면 객체 생성 시점에 스프링 컨테이너에서 해당 스프링 빈을 찾아서 주입한다.
@@ -26,19 +26,19 @@ public class MemberService {
     /** 회원가입 */
     public Long join(Member member) {
         //같은 이름이 있는 중복 회원x
-//        Optional<Member> result = memberRespository.findByName(member.getName());
+//        Optional<Member> result = memberRepository.findByName(member.getName());
 //        result.ifPresent(m -> {
 //            throw new IllegalStateException("이미 존재하는 회원입니다.");
 //        });
 
         validateDuplicateMember(member);    //중복 회원 검증
 
-        memberRespository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRespository.findByName(member.getName())
+        memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
@@ -46,11 +46,11 @@ public class MemberService {
 
     /** 전체 회원 조회 */
     public List<Member> findMembers(){
-        return memberRespository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
-        return memberRespository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 }
 
